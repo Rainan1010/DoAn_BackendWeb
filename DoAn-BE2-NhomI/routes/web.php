ae1c9c8ccf9cb3cff27ab42dad6323ea726711e1
ngnhanbao <baonhanltv3669@gmail.com>
2026-05-14 11:37:05 +0700

    Resolve conflict

--- DoAn-BE2-NhomI/routes/web.php
+++ DoAn-BE2-NhomI/routes/web.php
@@ -233,9 +233,6 @@ Route::get('/orders', [OrderController::class, 'history'])
 // xem chi tiet don hang
 Route::get('/orders/{id}', [OrderController::class, 'detail'])
     ->name('orders.detail');
-<<<<<<< HEAD
-
-
 /*
 |--------------------------------------------------------------------------
 | CHECKOUT
@@ -295,5 +292,4 @@ Route::middleware('auth')->group(function () {
         [OrderController::class, 'history']
     )->name('order.history');
 });
-=======
->>>>>>> master
+
